a7353aa8ff93701367c0600491fb83e8b0c9ba4f
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-01-08 14:53:09 +0600

    basic setup an react applection

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+const NavBar = () => {
+    return (
+        <div className=" flex justify-between items-center p-5">
+            <img src="https://i.ibb.co/2g8cLmX/timedoctor-logo.png" alt="" />
+            
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayOut from "../LayOut/MainLayOut";
+import Home from "../Pages/Home/Home";
+
+const Routes = createBrowserRouter([
+    {
+        path:"/",
+        element:<MainLayOut></MainLayOut>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+export default Routes
