afff416eb041e1247325d1e57855e649891a3d7d
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-01-08 15:41:56 +0600

    added to home banner, navbar and design register pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Routes from './Routes/Routes.jsx'
+import Provider from './Provider/Provider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className=' max-w-7xl mx-auto'>
-      <RouterProvider router={Routes}>
-      </RouterProvider>
+      <Provider>
+        <RouterProvider router={Routes}>
+        </RouterProvider>
+      </Provider>
     </div>
   </React.StrictMode>,
 )
